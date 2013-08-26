f194b07b85b7ddc41c8306cc2c9465fc97f0520b
Ignacio Sánchez <nacho@isholgueras.com>
2013-08-26 18:49:39 +0000

    integración del tema

--- sites/all/themes/custom/straightedge/templates/page.tpl.php
+++ sites/all/themes/custom/straightedge/templates/page.tpl.php
@@ -1,86 +1,84 @@
 <div id="header-wrapper">
   <div id="header" class="container">
     <div id="logo">
-      <h1><a href="#">Straightedge</a></h1>
+      <?php if (!$title): ?>
+        <h1><a href="<?php print $front_page; ?>">Straightedge</a></h1>
+      <?php else: /* Use h1 when the content title is empty */ ?>
+        <span class="title"><a href="<?php print $front_page; ?>">Straightedge</a></span>
+      <?php endif; ?>
     </div>
+    <?php if ($page['header']): ?>
     <div id="menu">
-      <ul>
-        <li class="current_page_item"><a href="#" accesskey="1" title="">Services</a></li>
-        <li><a href="#" accesskey="2" title="">Our Clients</a></li>
-        <li><a href="#" accesskey="3" title="">About Us</a></li>
-        <li><a href="#" accesskey="4" title="">Careers</a></li>
-        <li><a href="#" accesskey="5" title="">Contact Us</a></li>
-      </ul>
-    </div>
+        <?php print render($page['header']); ?>
+        <?php if ($main_menu): ?>
+          <?php
+          print theme('links__system_main_menu', array(
+              'links' => $main_menu,
+              'attributes' => array(
+                  'id' => 'main-menu-links',
+                  'class' => array('links', 'clearfix'),
+              ),
+          ));
+          ?>
+      <?php endif; ?>
+      </div>
+    <?php endif; ?>
   </div>
 </div>
-<div id="featured">&nbsp;</div>
-<div id="banner-wrapper">
-  <div id="banner" class="container">
-    <div class="box-left">
-      <h2>Mauris vulputate dolor nibh</h2>
-      <span>Donec leo, vivamus fermentum praesent urna congue rutrum</span> </div>
-    <div class="box-right"> <a href="#" class="button button-big">Etiam posuere</a></div>
+
+<?php if ($page['slideshow']): ?>
+  <div id="featured">
+    <?php print render($page['slideshow']); ?>
   </div>
-</div>
+<?php endif; ?>
+
+<?php if ($page['highlight']): ?>
+  <div id="banner-wrapper">
+    <?php print render($page['highlight']); ?>
+  </div>
+<?php endif; ?>
+
 <div id="wrapper">
   <div id="page" class="container">
-    <div id="content">
-      <div class="title">
-        <h2>Welcome to our website</h2>
-        <span class="byline">Mauris vulputate dolor sit amet nibh</span> </div>
-      <p>This is <strong>Straightedge</strong>, a free, fully standards-compliant CSS template designed by <a href="http://www.freecsstemplates.org/" rel="nofollow">FreeCSSTemplates.org</a>. The photos in this template are from <a href="http://fotogrph.com/"> Fotogrph</a>. This free template is released under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attributions 3.0</a> license, so you are pretty much free to do whatever you want with it (even use it commercially) provided you keep the links in the footer intact. Aside from that, have fun with it :) </p>
-      <div id="onecolumn">
-        <div class="title">
-          <h2>Pellentesque quis lectus gravida blandit</h2>
-        </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac quam risus, at tempus justo. Sed dictum rutrum massa eu volutpat. Quisque vitae hendrerit sem.</p>
-        <ul class="style2">
-          <li><a href="#">Semper mod quis eget mi dolore</a></li>
-          <li><a href="#">Quam turpis feugiat sit dolor</a></li>
-          <li><a href="#">Amet ornare in hendrerit in lectus</a></li>
-          <li><a href="#">Consequat etiam lorem phasellus</a></li>
-          <li><a href="#">Amet turpis, feugiat et sit amet</a></li>
-          <li><a href="#">Amet ornare in hendrerit in lectus</a></li>
-        </ul>
+
+    <?php if ($messages): ?>
+      <div id="messages"><div class="section clearfix">
+          <?php print $messages; ?>
+        </div></div> <!-- /.section, /#messages -->
+    <?php endif; ?>
+
+    <?php if ($title): ?>
+      <h1 class="title" id="page-title">
+        <?php print $title; ?>
+      </h1>
+    <?php endif; ?>
+        
+    <?php if ($page['content']): ?>
+      <div id="content">
+        <?php print render($page['content']); ?>
       </div>
-    </div>
+    <?php endif; ?>
+
     <div id="sidebar">
-      <ul class="style1">
-        <li class="first">
-          <h3>Amet sed volutpat mauris</h3>
-          <p><a href="#">In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</a></p>
-        </li>
-        <li>
-          <h3>Sagittis diam dolor sit amet</h3>
-          <p><a href="#">In posuere eleifend odio. Quisque semper augue mattis wisi. Pellentesque viverra vulputate enim. Aliquam erat volutpat.</a></p>
-        </li>
-      </ul>
+      <?php if ($page['full_sidebar']): ?>
+        <?php print render($page['full_sidebar']); ?>
+      <?php endif; ?>
+
       <div id="stwo-col">
-        <div class="sbox1">
-          <h2>Etiam rhoncus</h2>
-          <ul class="style2">
-            <li><a href="#">Semper quis egetmi dolore</a></li>
-            <li><a href="#">Quam turpis feugiat dolor</a></li>
-            <li><a href="#">Amet ornare hendrerit lectus</a></li>
-            <li><a href="#">Consequat lorem phasellus</a></li>
-            <li><a href="#">Amet turpis feugiat amet</a></li>
-          </ul>
-        </div>
-        <div class="sbox2">
-          <h2>Integer gravida</h2>
-          <ul class="style2">
-            <li><a href="#">Semper quis egetmi dolore</a></li>
-            <li><a href="#">Quam turpis feugiat dolor</a></li>
-            <li><a href="#">Amet ornare hendrerit lectus</a></li>
-            <li><a href="#">Consequat lorem phasellus</a></li>
-            <li><a href="#">Amet turpis feugiat amet</a></li>
-          </ul>
-        </div>
+        <?php if ($page['first_sidebar'] || $page['second_sidebar']): ?>
+          <div class="sbox1">
+            <?php print render($page['first_sidebar']); ?>
+          </div>
+          <div class="sbox2">
+            <?php print render($page['second_sidebar']); ?>
+          </div>
+        <?php endif; ?>
       </div>
+
     </div>
   </div>
 </div>
 <div id="copyright" class="container">
+  <?php print render($page['footer']); ?>
   <p>Copyright (c) 2013 Sitename.com. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://www.freecsstemplates.org/" rel="nofollow">FreeCSSTemplates.org</a>.</p>
 </div>
